40c5a40206ffcfbe4e86e54de301b218a7319a8c
Angelo Shema <geoangercola@gmail.com>
2020-12-23 18:01:12 +0200

    Renamed user into reservation

--- pages/ticket/[id].js
+++ pages/ticket/[id].js
@@ -1,20 +1,20 @@
 import { useEffect, useState } from "react";
 import qrcode from "qrcode";
-import { getUser } from "utils/fn/db";
+import { getReservation } from "utils/fn/db";
 
 import { SubmitButton } from "components/index"
 import Card from "components/card"
 
-let Ticket = ({ id, userPresent, user = {}, code, ...props }) => {
+let Ticket = ({ id, reservationPresent, reservation = {}, code, ...props }) => {
 	return (
 		<main>
 			<h1>Kibeho Sanctuary</h1>
 			<h2>Ticket</h2>
 			<div className="btn-bag"><a href="/">&larr; Register as attende</a></div>
-			{userPresent ?
+			{reservationPresent ?
 				<div>
-					<Card code={code} user={user} />
-					<a href={"/api/download-ticket.png?id="+user._id}><SubmitButton text="Download Ticket"/></a>
+					<Card code={code} reservation={reservation} />
+					<a href={"/api/download-ticket.png?id="+reservation._id} download={"ZIYA_Ticket_"+reservation._id+".png"}><SubmitButton text="Download Ticket"/></a>
 				</div>:
 				<div>ID not found!</div>
 			}
@@ -25,13 +25,13 @@ let Ticket = ({ id, userPresent, user = {}, code, ...props }) => {
 let btoa = (str) => Buffer.from(str).toString('base64');
 
 let getStaticProps = async ({ params: { id, ...elseP } }) => {
-	let userPresent = true;
-	let user = await getUser(id).catch(() => (userPresent = false));
+	let reservationPresent = true;
+	let reservation = await getReservation(id).catch(() => (reservationPresent = false));
 	let code = "";
-	if (userPresent) code = await qrcode.toString("https://ziya-ui.vercel.app/ticket/"+id, { type: "svg" });
-	// if (userPresent) code = await qrcode.toDataURL("https://ziya-ui.vercel.app/ticket/"+id);
+	if (reservationPresent) code = await qrcode.toString("https://ziya-ui.vercel.app/ticket/"+id, { type: "svg" });
+	// if (reservationPresent) code = await qrcode.toDataURL("https://ziya-ui.vercel.app/ticket/"+id);
 	return {
-		props: { id, user, userPresent, code },
+		props: { id, reservation, reservationPresent, code },
 	};
 };
 
